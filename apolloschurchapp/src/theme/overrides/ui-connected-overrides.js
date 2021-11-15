8dcd414f53b8c33e1c376d17ab94c143cfd2847f
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2021-11-15 08:52:32 -0600

    Remove override

--- apolloschurchapp/src/theme/overrides/ui-connected-overrides.js
+++ apolloschurchapp/src/theme/overrides/ui-connected-overrides.js
@@ -1,18 +1,6 @@
-import { FEATURE_FEED_ACTION_MAP } from '@apollosproject/ui-connected';
-import { useNavigation } from '@react-navigation/native';
-
 export default () => ({
   'ui-connected.RockAuthedWebBrowser': {
     primary: '#fff',
     paper: 'black',
-  },
-  'ui-connected.FeaturesFeedConnected': {
-    onPressActionItem: () => (action, ...props) => {
-      if (FEATURE_FEED_ACTION_MAP[action]) {
-        FEATURE_FEED_ACTION_MAP[action]({ action, ...props });
-      } else {
-        useNavigation.navigate('ContentFeed');
-      }
-    },
   },
 });
