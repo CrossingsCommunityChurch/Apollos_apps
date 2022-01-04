47a7e03c4c8280109d95a8edca0fc99dd137c084
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2022-01-04 10:11:36 -0600

    update app to 41

--- apolloschurchapp/src/index.js
+++ apolloschurchapp/src/index.js
@@ -80,11 +80,6 @@ const ThemedNavigationContainer = withTheme(({ theme, ...props }) => ({
   <NavigationContainer ref={containerRef} {...props} />
 ));
 
-const LandingToAuth = () => {
-  const navigation = useNavigation();
-  return <Landing onPressPrimary={() => navigation.navigate('Auth')} />;
-};
-
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const App = () => {
@@ -165,7 +160,7 @@ const App = () => {
                   stackPresentation: 'push',
                 }}
               />
-              <Screen name="LandingScreen" component={LandingToAuth} />
+              <Screen name="LandingScreen" component={Landing} />
               <Screen name="Search" component={SearchScreenConnected} />
               <Screen
                 name="UserSettingsNavigator"
